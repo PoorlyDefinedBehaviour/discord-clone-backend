839a2b336ff669a6f63d32abfe7f7e357940cfb3
Bruno <brunotj2015@hotmail.com>
2019-07-07 19:57:21 -0300

    adds database connection based on environment

--- src/database/CreateConnection.ts
+++ src/database/CreateConnection.ts
@@ -1,17 +1,24 @@
-import { createConnection, Connection } from "typeorm";
+import { createConnection, Connection, getConnectionOptions } from "typeorm";
 
-export default async (): Promise<Connection> =>
-  (process.env.ENVIRONMENT as string) === "DEV"
-    ? await createConnection({
-        type: "mongodb",
-        useNewUrlParser: true,
-        host: "localhost",
-        port: 27017,
-        database: "steakroastdev"
-      })
-    : await createConnection({
-        type: "mongodb",
-        useNewUrlParser: true,
-        url: process.env.MONGODB_URL,
-        database: "steakroastdev"
-      });
+export default async (): Promise<Connection> => {
+  const env: string = process.env.ENVIRONMENT as string;
+  console.log("env", env);
+  if (env === "DEV") {
+    return await createConnection(await getConnectionOptions("dev"));
+  }
+
+  if (env === "TEST") {
+    return await createConnection(await getConnectionOptions("test"));
+  }
+
+  if (env === "PROD") {
+    return await createConnection({
+      type: "mongodb",
+      useNewUrlParser: true,
+      url: process.env.MONGODB_URL,
+      database: "steakroastdev"
+    });
+  }
+
+  return {} as Connection;
+};
---
+++ src/database/migration/1562539293288-FirstMigration.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class FirstMigration1562539293288 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
