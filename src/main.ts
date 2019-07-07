094a3b64b0af70fd143b2e27d119b3c83693f9ec
Bruno <brunotj2015@hotmail.com>
2019-07-07 16:08:10 -0300

    adds express-graphql as a dependencie

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function main(): Promise<void> {
   await server.listen(process.env.PORT || 3000);
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 
+  server.get(
+    "/",
+    (_: express.Request, response: express.Response): express.Response =>
+      response.send("Hello world")
+  );
+
   process.on("unhandledRejection", (error: any): void => console.error(error));
   process.on("uncaughtException", (error: any): void => console.error(error));
 }
