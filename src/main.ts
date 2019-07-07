896d5fc212aad7042ad212dd9d600d6d04d8374a
Bruno <brunotj2015@hotmail.com>
2019-07-07 16:05:25 -0300

    removes hapijs, adds express

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import * as hapi from "hapi";
+import express from "express";
+import { config } from "dotenv";
+config();
 
 async function main(): Promise<void> {
-  const server: hapi.Server = new hapi.Server({
-    port: process.env.PORT || 3000,
-    host: process.env.HOST || "localhost"
-  });
+  const server: any = express();
 
-  await server.start();
-  console.log(`Server running on port ${process.env.port || 3000}`);
+  await server.listen(process.env.PORT || 3000);
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 
   process.on("unhandledRejection", (error: any): void => console.error(error));
   process.on("uncaughtException", (error: any): void => console.error(error));
