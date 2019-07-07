5e6a168db932a6abc8ae7ae071df81e4e1e94143
Bruno <brunotj2015@hotmail.com>
2019-07-07 19:25:46 -0300

    setups connection with local mongodb

--- src/database/CreateConnection.ts
+++ src/database/CreateConnection.ts
@@ -1,12 +1,17 @@
 import { createConnection, Connection } from "typeorm";
 
-export default async (): Promise<Connection> => {
-  const connection: Connection = await createConnection({
-    type: "mongodb",
-    useNewUrlParser: true,
-    url: process.env.MONGODB_URL,
-    database: "steakroastdev"
-  });
-
-  return connection;
-};
+export default async (): Promise<Connection> =>
+  (process.env.ENVIRONMENT as string) === "DEV"
+    ? await createConnection({
+        type: "mongodb",
+        useNewUrlParser: true,
+        host: "localhost",
+        port: 27017,
+        database: "steakroastdev"
+      })
+    : await createConnection({
+        type: "mongodb",
+        useNewUrlParser: true,
+        url: process.env.MONGODB_URL,
+        database: "steakroastdev"
+      });
--- src/database/entity/User.ts
+++ src/database/entity/User.ts
@@ -6,11 +6,11 @@ export class User {
   id: number;
 
   @Column()
-  firstName: string;
+  username: string;
 
   @Column()
-  lastName: string;
+  password: string;
 
   @Column()
-  age: number;
+  domain: string;
 }
