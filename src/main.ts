8286cc432abb690b044dbbc71b44a46057dab01b
Bruno <brunotj2015@hotmail.com>
2019-07-07 15:54:38 -0300

    initial hapi server setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import * as hapi from "@hapi/hapi";
+
+async function main(): Promise<void> {
+  const server: any = hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || "localhost"
+  });
+
+  await server.start();
+  console.log(`Server running on port ${process.env.port || 3000}`);
+
+  process.on("unhandledRejection", (error: any): void => console.error(error));
+  process.on("uncaughtException", (error: any): void => console.error(error));
+}
+main();
