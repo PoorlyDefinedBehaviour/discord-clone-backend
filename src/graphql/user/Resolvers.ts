18378a1a5dcf2c0acac75609da00bf82a920d347
Bruno <brunotj2015@hotmail.com>
2019-07-08 01:21:25 -0300

    adds email andf password validation for register mutation

--- src/graphql/user/Resolvers.ts
+++ src/graphql/user/Resolvers.ts
@@ -1,5 +1,19 @@
+import * as yup from "yup";
+import { FormatYupError } from "../../utils/FormatYupError";
 import User from "../../database/entity/User";
 import create_connection from "../../database/CreateConnection";
+
+const RegisterSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(5, "Email must be at least 5 characters long")
+    .max(255)
+    .email("Email must be a valid email"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(255)
+});
 
 export default {
   Query: {
@@ -11,12 +25,40 @@ export default {
       _: any,
       { email, password }: GQL.IRegisterOnMutationArguments
     ) => {
-      const user = new User();
-      user.email = email;
-      user.password = password;
+      try {
+        await RegisterSchema.validate(
+          { email, password },
+          { abortEarly: false }
+        );
+      } catch (error) {
+        return FormatYupError(error);
+      }
 
-      const connection = await create_connection();
-      return await connection.mongoManager.save(user);
+      try {
+        const connection = await create_connection();
+
+        if (!!(await connection.mongoManager.findOne(User, { email }))) {
+          return [
+            {
+              status: 422,
+              path: "email",
+              message: "Email already in use"
+            }
+          ];
+        }
+
+        await connection.mongoManager.save(new User(email, password));
+        return [{ status: 201, message: "User created" }];
+      } catch (error) {
+        console.error(error);
+        return [
+          {
+            status: 500,
+            path: null,
+            error: "Something went wrong trying to create a new user"
+          }
+        ];
+      }
     }
   }
 };
